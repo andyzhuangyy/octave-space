0b941aea0ae33743a0bb96531708773454ec890c
zhuangyongyao <andyzyy@gmail.com>
2016-09-04 09:47:49 +0800

    complete ex2 homework

--- ml-class/machine-learning-ex2/ex2/costFunctionReg.m
+++ ml-class/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% base = ones(size(theta));
+% base(1) = 0;
 
+h = sigmoid(X * theta);
 
+theta(1) = 0;
 
+J = 1/m * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda/(2 * m) * theta' * theta ;
 
+grad = 1/m * (X' * (h - y)) + lambda / m * theta;
 
 % =============================================================
 
